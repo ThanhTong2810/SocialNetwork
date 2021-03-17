f75f71fe2cda0db2a6a95699be72fca714624ec5
ThanhTong2810 <tong.duong@kthub.dev>
2021-03-17 20:39:13 +0700

    16 post method

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded());
+app.use(express.json());
+
 //HTTP Logger
 app.use(morgan('combined'));
 
@@ -19,12 +22,20 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  res.send('');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
