61eeea8b531103bb401510b26603b9e5be2861a6
ThanhTong2810 <tong.duong@kthub.dev>
2021-03-17 21:35:32 +0700

    18 routes and controllers

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,14 @@
+class NewsController{
+
+    //[GET] /news
+    news(req,res){
+        res.render('news');
+    }
+
+    //[GET] /news/:slug
+    show(req, res){
+        res.send('NEWS DETAIL');
+    }
+}
+
+module.exports=new NewsController;
---
+++ src/routes/news.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const newsController = require('../app/controllers/NewsController');
+
+router.use('/:slug', newsController.show);
+router.use('/', newsController.news);
+
+
+module.exports = router;
